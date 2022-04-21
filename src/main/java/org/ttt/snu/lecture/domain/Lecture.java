71fd0b341b77437c5a304c23d192b10b4cb5f8cc
jinki <ian1041@gmail.com>
2022-04-21 11:17:19 +0900

    교수 관련 DTO 작성

--- src/main/java/org/ttt/snu/lecture/domain/Lecture.java
+++ src/main/java/org/ttt/snu/lecture/domain/Lecture.java
@@ -1,5 +1,163 @@
 package org.ttt.snu.lecture.domain;
 
 public class Lecture {
+	private String lCode;
+	private String lType;
+	private String lName;
+	private String lObjective;
+	private String lContents;
+	private String textbook;
+	private int lDays;
+	private int credit;
+	private int lYear;
+	private int lSemester;
+	private int lCapacity;
+	private String lStatus;
+	private String pCode;
+	private String dCode;
+	
+	public Lecture() {}
+
+	public Lecture(String lCode, String lType, String lName, String lObjective, String lContents, String textbook,
+			int lDays, int credit, int lYear, int lSemester, int lCapacity, String lStatus, String pCode,
+			String dCode) {
+		super();
+		this.lCode = lCode;
+		this.lType = lType;
+		this.lName = lName;
+		this.lObjective = lObjective;
+		this.lContents = lContents;
+		this.textbook = textbook;
+		this.lDays = lDays;
+		this.credit = credit;
+		this.lYear = lYear;
+		this.lSemester = lSemester;
+		this.lCapacity = lCapacity;
+		this.lStatus = lStatus;
+		this.pCode = pCode;
+		this.dCode = dCode;
+	}
+
+	public String getlCode() {
+		return lCode;
+	}
+
+	public void setlCode(String lCode) {
+		this.lCode = lCode;
+	}
+
+	public String getlType() {
+		return lType;
+	}
+
+	public void setlType(String lType) {
+		this.lType = lType;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getlObjective() {
+		return lObjective;
+	}
+
+	public void setlObjective(String lObjective) {
+		this.lObjective = lObjective;
+	}
+
+	public String getlContents() {
+		return lContents;
+	}
+
+	public void setlContents(String lContents) {
+		this.lContents = lContents;
+	}
+
+	public String getTextbook() {
+		return textbook;
+	}
+
+	public void setTextbook(String textbook) {
+		this.textbook = textbook;
+	}
+
+	public int getlDays() {
+		return lDays;
+	}
+
+	public void setlDays(int lDays) {
+		this.lDays = lDays;
+	}
+
+	public int getCredit() {
+		return credit;
+	}
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public int getlYear() {
+		return lYear;
+	}
+
+	public void setlYear(int lYear) {
+		this.lYear = lYear;
+	}
+
+	public int getlSemester() {
+		return lSemester;
+	}
+
+	public void setlSemester(int lSemester) {
+		this.lSemester = lSemester;
+	}
+
+	public int getlCapacity() {
+		return lCapacity;
+	}
+
+	public void setlCapacity(int lCapacity) {
+		this.lCapacity = lCapacity;
+	}
+
+	public String getlStatus() {
+		return lStatus;
+	}
+
+	public void setlStatus(String lStatus) {
+		this.lStatus = lStatus;
+	}
+
+	public String getpCode() {
+		return pCode;
+	}
+
+	public void setpCode(String pCode) {
+		this.pCode = pCode;
+	}
+
+	public String getdCode() {
+		return dCode;
+	}
+
+	public void setdCode(String dCode) {
+		this.dCode = dCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Lecture [lCode=" + lCode + ", lType=" + lType + ", lName=" + lName + ", lObjective=" + lObjective
+				+ ", lContents=" + lContents + ", textbook=" + textbook + ", lDays=" + lDays + ", credit=" + credit
+				+ ", lYear=" + lYear + ", lSemester=" + lSemester + ", lCapacity=" + lCapacity + ", lStatus=" + lStatus
+				+ ", pCode=" + pCode + ", dCode=" + dCode + "]";
+	}
+	
+	
 
 }
--- src/main/java/org/ttt/snu/professor/domain/Professor.java
+++ src/main/java/org/ttt/snu/professor/domain/Professor.java
@@ -1,5 +1,134 @@
 package org.ttt.snu.professor.domain;
 
+import java.sql.Date;
+
 public class Professor {
+	
+	private String pCode;
+	private String pName;
+	private String pPassword;
+	private String pRrn;
+	private String pAddress;
+	private String pEmail;
+	private String pGender;
+	private String pPhone;
+	private Date pAddmission;
+	private String advisorYN;
+	private String dCode;
+	
+	public Professor() {}
+	
+	public Professor(String pCode, String pName, String pPassword, String pRrn, String pAddress, String pEmail,
+			String pGender, String pPhone, Date pAddmission, String advisorYN, String dCode) {
+		super();
+		this.pCode = pCode;
+		this.pName = pName;
+		this.pPassword = pPassword;
+		this.pRrn = pRrn;
+		this.pAddress = pAddress;
+		this.pEmail = pEmail;
+		this.pGender = pGender;
+		this.pPhone = pPhone;
+		this.pAddmission = pAddmission;
+		this.advisorYN = advisorYN;
+		this.dCode = dCode;
+	}
+
+	public String getpCode() {
+		return pCode;
+	}
+
+	public void setpCode(String pCode) {
+		this.pCode = pCode;
+	}
+
+	public String getpName() {
+		return pName;
+	}
+
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	public String getpPassword() {
+		return pPassword;
+	}
+
+	public void setpPassword(String pPassword) {
+		this.pPassword = pPassword;
+	}
+
+	public String getpRrn() {
+		return pRrn;
+	}
+
+	public void setpRrn(String pRrn) {
+		this.pRrn = pRrn;
+	}
+
+	public String getpAddress() {
+		return pAddress;
+	}
+
+	public void setpAddress(String pAddress) {
+		this.pAddress = pAddress;
+	}
+
+	public String getpEmail() {
+		return pEmail;
+	}
+
+	public void setpEmail(String pEmail) {
+		this.pEmail = pEmail;
+	}
+
+	public String getpGender() {
+		return pGender;
+	}
+
+	public void setpGender(String pGender) {
+		this.pGender = pGender;
+	}
+
+	public String getpPhone() {
+		return pPhone;
+	}
+
+	public void setpPhone(String pPhone) {
+		this.pPhone = pPhone;
+	}
+
+	public Date getpAddmission() {
+		return pAddmission;
+	}
+
+	public void setpAddmission(Date pAddmission) {
+		this.pAddmission = pAddmission;
+	}
+
+	public String getAdvisorYN() {
+		return advisorYN;
+	}
+
+	public void setAdvisorYN(String advisorYN) {
+		this.advisorYN = advisorYN;
+	}
+
+	public String getdCode() {
+		return dCode;
+	}
+
+	public void setdCode(String dCode) {
+		this.dCode = dCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Professor [pCode=" + pCode + ", pName=" + pName + ", pPassword=" + pPassword + ", pRrn=" + pRrn
+				+ ", pAddress=" + pAddress + ", pEmail=" + pEmail + ", pGender=" + pGender + ", pPhone=" + pPhone
+				+ ", pAddmission=" + pAddmission + ", advisorYN=" + advisorYN + ", dCode=" + dCode + "]";
+	}
+	
+	
 
 }
