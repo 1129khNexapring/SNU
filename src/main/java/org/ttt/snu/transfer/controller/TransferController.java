13900caca4788c2873849a14983bcd518edc924a
YOUM <YOUM@183.100.229.219>
2022-04-29 14:09:22 +0900

    휴학컨트롤러 추가

--- src/main/java/org/ttt/snu/transfer/controller/TransferController.java
+++ src/main/java/org/ttt/snu/transfer/controller/TransferController.java
@@ -3,13 +3,11 @@ package org.ttt.snu.transfer.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.ttt.snu.department.domain.Department;
 import org.ttt.snu.student.domain.Student;
-import org.ttt.snu.transfer.domain.Transfer;
 import org.ttt.snu.transfer.service.TransferService;
 
 import com.nexacro.uiadapter17.spring.core.annotation.ParamVariable;
