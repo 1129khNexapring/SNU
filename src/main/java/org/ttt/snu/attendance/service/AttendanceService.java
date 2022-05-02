00c9e50e1ae09037b61bf4dd154152c9e31a3ad1
HyoGwang <HyoGwang@192.168.64.2>
2022-05-02 10:35:21 +0900

    출결 관리 교과목명 리스트 조회 - 효광

--- src/main/java/org/ttt/snu/attendance/service/AttendanceService.java
+++ src/main/java/org/ttt/snu/attendance/service/AttendanceService.java
@@ -3,9 +3,11 @@ package org.ttt.snu.attendance.service;
 import java.util.List;
 
 import org.ttt.snu.attendance.domain.Attendance;
+import org.ttt.snu.lecture.domain.Lecture;
 
 public interface AttendanceService {
 	
+	public List<Lecture> printLecture();
 	public List<Attendance> printAll(Attendance attendance);
 	
 	public int registerAttendance(Attendance attendance);
--- src/main/java/org/ttt/snu/attendance/store/AttendanceStore.java
+++ src/main/java/org/ttt/snu/attendance/store/AttendanceStore.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.ttt.snu.attendance.domain.Attendance;
+import org.ttt.snu.lecture.domain.Lecture;
 
 public interface AttendanceStore {
 	
+	public List<Lecture> selectLectureAll(SqlSession sqlSession);
 	public List<Attendance> selecAll(SqlSession sqlSession, Attendance attendance);
 	
 	public int insertAttendance(SqlSession sqlSession, Attendance attendance);
 	public int updateAttendance(SqlSession sqlSession, Attendance attendance);
 //	public int deleteAttendance(SqlSession sqlSession);
+
 }
