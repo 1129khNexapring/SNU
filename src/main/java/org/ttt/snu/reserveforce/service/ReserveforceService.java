22a6d9e4a0507ff28c72977c7b059de3227b91a1
User <User@211.218.245.83>
2022-05-04 03:20:10 +0900

    //예비군 조회&승인 완료

--- src/main/java/org/ttt/snu/reserveforce/service/ReserveforceService.java
+++ src/main/java/org/ttt/snu/reserveforce/service/ReserveforceService.java
@@ -1,5 +1,22 @@
 package org.ttt.snu.reserveforce.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ttt.snu.reserveforce.domain.Reserveforce;
+import org.ttt.snu.student.domain.Student;
+
 public interface ReserveforceService {
+
+	List<Reserveforce> printRfList();
+
+	List<Student> printStudent();
+
+	int modifyStatus(Reserveforce reserveforce);
+
+//	List<HashMap<String, Object>> printStudent();
+
+	
 
 }
--- src/main/java/org/ttt/snu/reserveforce/store/ReserveforceStore.java
+++ src/main/java/org/ttt/snu/reserveforce/store/ReserveforceStore.java
@@ -1,5 +1,25 @@
 package org.ttt.snu.reserveforce.store;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.ttt.snu.reserveforce.domain.Reserveforce;
+import org.ttt.snu.student.domain.Student;
+
 public interface ReserveforceStore {
+
+	List<Reserveforce> selectRfList(SqlSession sqlSession);
+
+	List<Student> selectStudent(SqlSession sqlSession);
+
+	int updateStatus(SqlSession sqlSession, Reserveforce reserveforce);
+
+//	List<HashMap<String, Object>> selectStudent(SqlSession sqlSession);
+
+	
+
+	
 
 }
