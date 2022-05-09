08c3bfecf2dfe910bc0a60bedddfb9f7a2aab8db
eorhk <eorhk@DESKTOP-P5TS50Q>
2022-05-09 17:57:13 +0900

    스케줄 등록 완료

--- src/main/java/org/ttt/snu/academicCaldedar/controller/AcademicCalendarController.java
+++ src/main/java/org/ttt/snu/academicCaldedar/controller/AcademicCalendarController.java
@@ -1,17 +1,11 @@
 package org.ttt.snu.academicCaldedar.controller;
 
 import java.sql.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -63,6 +57,6 @@ public class AcademicCalendarController {
 			
 		}
 		
-	}//커밋용 수정
+	}
 	
 }
