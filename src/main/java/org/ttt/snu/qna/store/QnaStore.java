16ff3cddadc74d926766a4e631b639c871597f44
YOUM <YOUM@192.168.219.101>
2022-05-18 17:03:03 +0900

    건의게시판 생성 - 동현

--- src/main/java/org/ttt/snu/qna/store/QnaStore.java
+++ src/main/java/org/ttt/snu/qna/store/QnaStore.java
@@ -1,5 +1,16 @@
 package org.ttt.snu.qna.store;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.ttt.snu.qna.domain.Qna;
+
 public interface QnaStore {
+
+	List<Qna> printAllQna(SqlSession sqlSession);
+
+	int updateCount(SqlSession sqlSession, Qna qna);
+
+	int registerQna(SqlSession sqlSession, Qna qna);
 
 }
