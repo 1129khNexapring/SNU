32a6387e7c62ed35eaf1479abe4e140ca24bedef
daekwang8282 <eorhkd8282@gmail.com>
2022-05-18 17:16:38 +0900

    학사스케줄 완료

--- src/main/java/org/ttt/snu/academicCaldedar/controller/AcademicCalendarController.java
+++ src/main/java/org/ttt/snu/academicCaldedar/controller/AcademicCalendarController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.ttt.snu.academicCaldedar.domain.AcademicCalendar;
 import org.ttt.snu.academicCaldedar.service.AcademicCalendarService;
 
